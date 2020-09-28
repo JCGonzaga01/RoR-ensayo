9a0e40742b38ef8fac0da62e77c65af8a4cc07e9
John Christopher Gonzaga <johnchristopher.gonzaga@finastra.com>
2020-09-28 18:50:45 +0800

    1. Define different routings syntax in routes.rb 2. Define how to explicitly render views from controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,14 @@ class DemoController < ApplicationController
   layout false 
   
   def index
+    # Explicit Render of Views. 
+    # By default RoR wil take the controller and method name to look for views html
+    # but we can explicitly specify if we want to render different views from the method
+    # syntax: render('{directory goes here, root is views folder}')
+    render('demo/index')
+  end
+
+  def hello 
+    render('hello')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  # ROOT ROUTE
+  # The home page or the root route to be called when app is called
+  root "demo#index"
+  match "/" => "demo#index", :via => :get
+
+  # SIMPLE ROUTE
+  # Get is specified to any routes call the use get https
+  get 'demo/index' => "demo#index"
+  # while Match is specified to run the routes to any given http apis using ":via => [(array of http apis)]"
+  # match "index" => "demo#index", :via => [:get]
+
+  get "demo/hello"
+
+  # DEFAULT ROUTE
+  # ( ) = means optional in routes link.
+  # get ':controller(/:action(/:id))'
+  # # or
+  # match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
